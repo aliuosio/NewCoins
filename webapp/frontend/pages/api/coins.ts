4db03c9d7cbd14b3a8d8c1ca78f4352a33198845
Osiozekhai Aliu <oaliu@hotmail.com>
2025-04-21 11:24:04 +0200

    modified frontend for new paths

--- webapp/frontend/pages/api/coins.ts
+++ webapp/frontend/pages/api/coins.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Use Docker service name for backend if running in Docker Compose, else fallback to localhost
-  const backendUrl = process.env.BACKEND_URL || 'http://pad_webapp_backend:8000/api/coins';
+  const backendUrl = process.env.BACKEND_URL_COINS || 'http://nc_webapp_backend:8000/api/coins';
   try {
     const response = await fetch(backendUrl);
     if (!response.ok) {
