2680bedd521848854156094056deffb40971ca9c
Osiozekhai Aliu <oaliu@hotmail.com>
2025-04-29 01:37:25 +0200

    moved requirementsto docker

--- cron.sh
+++ cron.sh
@@ -6,11 +6,6 @@ cd "$(dirname "$0")"
 # Start Docker Compose in detached mode
 docker compose up -d
 
-# Wait 45 seconds for containers to be ready (countdown)
-for i in {45..1}; do
-  printf "\rWaiting for Python Dependencies to load: %2d seconds remaining..." "$i"
-  sleep 1
-done
 printf "\rContainers should be ready!                   \n"
 
 # Run the scheduler inside the python container
